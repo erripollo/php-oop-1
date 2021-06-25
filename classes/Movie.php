be1bb8a593bb7740544f1e5faafb9880daba5dda
erripollo <pollone.enrico@gmail.com>
2021-06-25 16:54:25 +0200

    add 2 methods in Movie class

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,11 +1,18 @@
 <?php 
 
+
+    /**
+     * Movie class
+     * @author Enrico <email@email.com>
+     * @copyright 2021 Enrico
+     */
     class Movie
     {
         public $title;
         public $language;
         public $overview;
         public $relaseDate;
+        public $vote;
 
 
         /**
@@ -15,13 +22,39 @@
          * @param string $language
          * @param string $overview
          * @param string $relaseDate
+         * @param 
          */
-        function __construct(strin $title, string $language, string $overview, string $relaseDate)
+        function __construct(string $title, string $language, string $overview, string $relaseDate, int $vote)
         {
             $this->title = $title;
             $this->language = $language;
             $this->overview = $overview;
             $this->relaseDate = $relaseDate;
+            $this->vote = $vote;
+        }
+
+        /**
+         * ## getVote
+         * get movie vote
+         *
+         * @return void $vote Movie vote
+         */
+        public function getVote()
+        {
+            return $this->vote;
+        }
+
+
+        /**
+         * ## setVote
+         * set movie vote
+         *
+         * @param integer $vote
+         * @return void
+         */
+        public function setVote(int $vote)
+        {
+            $this->vote = $vote;
         }
     }
 
