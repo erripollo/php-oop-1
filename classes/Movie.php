2ae2cee7546821514a02930be50cce824ffff923
erripollo <pollone.enrico@gmail.com>
2021-06-25 16:25:17 +0200

    add constructor in class Movie

--- classes/Movie.php
+++ classes/Movie.php
@@ -7,7 +7,22 @@
         public $overview;
         public $relaseDate;
 
-        
+
+        /**
+         * Movie function
+         *
+         * @param strin $title
+         * @param string $language
+         * @param string $overview
+         * @param string $relaseDate
+         */
+        function __construct(strin $title, string $language, string $overview, string $relaseDate)
+        {
+            $this->title = $title;
+            $this->language = $language;
+            $this->overview = $overview;
+            $this->relaseDate = $relaseDate;
+        }
     }
 
 ?>
